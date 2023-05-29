f6217d00a7cbab42a5edac6e55d6d4756ff09122
VITALY <9004673@mail.ru>
2023-05-29 17:06:15 +0300

    1 comit

---
+++ src/main/java/controllers/HelloController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        System.out.println("Hello World");
+        model.addAttribute("message", "Hello World!");
+        return "hello";
+    }
+
+
+}
+
